a11f68261d7953480d39c85346abe57bcb9a94cd
Julien Bachmann <julien.bachmann@webdoc.com>
2012-06-24 16:39:21 +0200

    initial commit

---
+++ Site_de_Julien_Bachmann/Welcome_files/Welcome.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120624
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
